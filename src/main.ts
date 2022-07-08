94a8dc9ba2f27e5ac21c27c728f2f66e70ddc66a
Gabriel <hinoago@gmail.com>
2022-07-08 00:56:08 -0300

    testing some new features

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,28 @@
+import { PageController } from "./controller/PageController";
 import { Gerenciamento } from "./Gerenciamento";
 import { Musico } from "./model/Musico";
 
 const gerenciamento = new Gerenciamento();
 
-const generos = ["power metal", "neoclassic metal"];
-const instrumentos = ["guitarra", "teclado"];
+const generos = ["rock", "metal"];
+const instrumentos = ["bateria", "guitarra"];
 
-const musico = new Musico("Gabriel", "hinoago@gmail.com", generos, instrumentos);
+const musico = new Musico("Fulano", "fulano@ru.co", generos, instrumentos);
 gerenciamento.cadastrar(musico);
 console.log(gerenciamento.getMusicos());
 
-gerenciamento.adicionarInstrumento("hinoago@gmail.com", "tin whistle");
-console.log(gerenciamento.getMusicos());
+PageController.getAddButton().addEventListener("click", () =>{
+    console.log("cadastrar");
+});
+
+PageController.getSearchButton().addEventListener("click", () =>{
+    console.log("buscar");
+});
+
+PageController.getInsertButton().addEventListener("click", () =>{
+    console.log("inserir");
+});
+
+PageController.getRemoveButton().addEventListener("click", () =>{
+    console.log("remover");
+});
