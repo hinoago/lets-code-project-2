384ffd420fa4bcd9218f8cd3062f7192ce1fcdc3
F3547441 Gabriel Oliveira da Silva Sergio <gabriel.sergio@bb.com.br>
2022-07-08 13:01:20 -0300

    created modal class

---
+++ src/utils/Utils.ts
@@ -0,0 +1,24 @@
+class Utils{
+    static createModal(modalContent: string, closeButtonText: string){
+        const modal = document.createElement("div");
+        const background = document.createElement("div");
+        const button = document.createElement("button");
+
+        modal.innerHTML = modalContent;
+        button.innerText = closeButtonText;
+        button.addEventListener("click", this.removeModal);
+        modal.id = "modal";
+        background.id = "background";
+        button.id = "btn_modal";
+        modal.appendChild(button);
+        background.appendChild(modal);
+        document.body.appendChild(background);
+    }
+    
+    static removeModal(){
+        document.querySelector("#background")?.remove();
+        document.querySelector("#modal")?.remove();
+    }
+}
+
+export {Utils}
